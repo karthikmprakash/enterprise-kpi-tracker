911f285576241f2474e4b470f10156415aa94f7d
Karthik M Prakash <karthikmprakash@192.168.0.234>
2024-09-29 16:15:57 +0530

    added basic KPI dashboard

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,13 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    data = {
+        "username": "Karthik",
+        "date": datetime.today(),
+        "tasks": {"completed": 2, "requires_attention": 5, "pending": 10},
+    }
+    return render(request, "home.html", context=data)
